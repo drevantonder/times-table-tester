5e630705c3cb7b17434854042d2accfa295c84cc
Andre van Tonder <drevtonder@gmail.com>
2018-07-23 15:41:32 +0800

    📐 add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
 
   <div id="app">
+    <nav id="navbar" class="level has-background-white">
+      <div class="level-item has-text-centered">
+        <div>
+          <router-link to="/">Game</router-link>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <router-link to="/leaderboard">LeaderBoard</router-link>
+        </div>
+      </div>    
+    </nav>  
     <div id="background" />
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -10,6 +22,8 @@
 #app {
   height: 100vh;
   width: 100%;
+
+  padding-top: 2rem;
 }
 
 #background {
@@ -17,7 +31,18 @@
   width: 100%;
   position: fixed;
 
+  top: 0;
+
+  z-index: -1000;
+
   background-color: #08aeea;
   background-image: linear-gradient(151deg, #08aeea 0%, #2af598 100%);
+}
+
+#navbar {
+  position: fixed;
+
+  top: 0;
+  width: 100%;
 }
 </style>
