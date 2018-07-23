efb5ed108a85aceb8ae421365ad4e7b5de7ca067
Andre van Tonder <drevtonder@gmail.com>
2018-07-23 14:58:01 +0800

    🌈 fix background

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
 
   <div id="app">
+    <div id="background" />
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
+#background {
   height: 100vh;
+  width: 100%;
+  position: fixed;
 
   background-color: #08aeea;
   background-image: linear-gradient(151deg, #08aeea 0%, #2af598 100%);
