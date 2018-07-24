7dc4390f11ac049f266c3741c0db4faefcb12f24
Andre van Tonder <drevtonder@gmail.com>
2018-07-24 09:21:04 +0800

    🔥 add firebase config

---
+++ src/config/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyACUMNB19npttIqfClOzoi1KrPRYwdkK14",
+  authDomain: "times-table-tester.firebaseapp.com",
+  databaseURL: "https://times-table-tester.firebaseio.com",
+  projectId: "times-table-tester",
+  storageBucket: "times-table-tester.appspot.com",
+  messagingSenderId: "545642185996"
+};
